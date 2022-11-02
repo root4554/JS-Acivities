d854d14380ef034b854e72bb74b2fad13af224b4
root4554 <anass.kbir@hotmail.com>
2022-11-02 10:04:42 +0100

    object example

---
+++ Cartas/Ej1Animales/Animales.js
@@ -0,0 +1,31 @@
+var sAnimales = `
+[
+    {
+        "Nombre": "Elefante",
+        "Mamifero": "true"
+    },
+    {
+        "Nombre": "Colibrí",
+        "Mamifero": "false"
+    },
+    {
+        "Nombre": "Buho",
+        "Mamifero": "false"
+    },
+    {
+        "Nombre": "Guepardo",
+        "Mamifero": "true"
+    },
+    {
+        "Nombre": "Caballo",
+        "Mamifero": "true"
+    },
+    {
+        "Nombre": "Avestruz",
+        "Mamifero": "false"
+    }
+]`;
+
+// aAnimales = JSON.parse(sAnimales)
+// aAnimales.forEach((x) => console.log(x.Nombre));
+// JSON.parse(sAnimales).filter(animal => animal.Mamifero == "true")
---
+++ Cartas/Ej1Animales/Datos.js
@@ -0,0 +1,26 @@
+[
+  {
+    Nombre: "Elefante",
+    Mamifero: "true",
+  },
+  {
+    Nombre: "Colibrí",
+    Mamifero: "false",
+  },
+  {
+    Nombre: "Buho",
+    Mamifero: "false",
+  },
+  {
+    Nombre: "Guepardo",
+    Mamifero: "true",
+  },
+  {
+    Nombre: "Caballo",
+    Mamifero: "true",
+  },
+  {
+    Nombre: "Avestruz",
+    Mamifero: "false",
+  },
+];
